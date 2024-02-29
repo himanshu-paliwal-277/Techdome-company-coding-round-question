24808c0632687a311c36885e3c2816e88fa3cfe5
Himanshu Paliwal <hpaliwal364@gmail.com>
2024-02-29 19:17:11 +0530

    Program1 solution is added

--- program1.js
+++ program1.js
@@ -2,10 +2,64 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
-    
+var isValid = function (s) {
+  let arr = [];
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(" || s[i] === "{" || s[i] === "[") {
+      arr.push(s[i]);
+    } else {
+      let lastElement = arr[arr.length - 1];
+      if (
+        (lastElement === "(" && s[i] === ")") ||
+        (lastElement === "{" && s[i] === "}") ||
+        (lastElement === "[" && s[i] === "]")
+      ) {
+        arr.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  if (arr.length === 0) {
+    return true;
+  }
+  return false;
 };
 
-module.exports = { isValid };
+// test case 1:
+console.log(isValid("()")); 
+// Output: true
+// Expected: true
+
+// test case 2:
+console.log(isValid("()[]{}"));
+// Output: true
+// Expected: true
+
+// test case 3:
+console.log(isValid("{[()]}"));
+// Output: true
+// Expected: true
+
+// test case 4:
+console.log(isValid("(]"));
+// Output: false
+// Expected: false
+
+// test case 5:
+console.log(isValid("([)]"));
+// Output: false
+// Expected: false
+
+// test case 6:
+console.log(isValid(""));
+// Output: true
+// Expected: true
+
+// test case 7:
+console.log(isValid("(){"));
+// Output: false
+// Expected: false
 
 
+// module.exports = { isValid };
